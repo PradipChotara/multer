c05c48c7f5d085c7e399f3d8c84ac80f7fde5805
pradip <pradip.chotara@aeonx.digital>
2024-05-18 13:11:04 +0530

    saving folders

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 // Import express
 const express = require("express");
 const multer = require("multer");
+const fs = require("fs");
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./uploads");
+    const currentDate = getCurrentDateTime();
+    const folderPath = `./uploads/${currentDate}`;
+    fs.mkdirSync(folderPath, { recursive: true });
+    cb(null, folderPath);
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
@@ -36,3 +40,16 @@ app.post("/upload", upload.single("myFile"), (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server started on port : ${PORT}`);
 });
+
+function getCurrentDateTime() {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, '0');
+  const day = String(now.getDate()).padStart(2, '0');
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
+  const milliseconds = String(now.getMilliseconds()).padStart(4, '0');
+
+  return `${year}-${month}-${day}-${hours}-${minutes}-${seconds}-${milliseconds}`;
+}
